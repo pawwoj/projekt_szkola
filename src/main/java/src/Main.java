6277a6a7c81c0b74ca1db5cebf6c7b69aa840011
pawwoj <pwojdacki84@gmail.com>
2021-10-01 19:12:54 +0200

    Nowy komentarz dotyczacy pushowania galazki

---
+++ src/main/java/src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+// git branch nazwa_galezi - utworzenie nowej galezi
+// git checkout - przelaczenie sie na galaz
+// git push --set-upstream origin szablon-projektu -> wypychanie galazki za pierwszym razem to taka komenda
+public class Main {
+    public static void main(String []args){
+        System.out.println("Hello world!");
+    }
+}
