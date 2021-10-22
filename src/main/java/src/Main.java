12ae20df2b57d383400e81f813304ffb05730641
pawwoj <pwojdacki84@gmail.com>
2021-10-22 20:46:58 +0200

    z5 remove student added

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -8,3 +8,21 @@ public class Main {
         app.run();
     }
 }
+
+/*
+1 Jan Kowalski
+3 Imie Nazwisko     ->     Lekcja: [1 , 2]
+4 Imie2 Nazwisko2
+
+1 Lekcja1
+2 Lekcja3
+ */
+
+/*
+Lekcja 5
+- poprawa main (else if wszedzie wolasz 1 metode)
+- persystencja w ID obiektow
+
+
+*sprawdzenie testow
+ */
