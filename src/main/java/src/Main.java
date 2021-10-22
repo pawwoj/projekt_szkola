bb9c600bd46711dd61a9bd0223a748b219be14e9
pawwoj <pwojdacki84@gmail.com>
2021-10-22 20:52:22 +0200

    New comment

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -20,9 +20,10 @@ public class Main {
 
 /*
 Lekcja 5
+- wprowadzic zadanie domowe jako osobne zadania w github board
 - poprawa main (else if wszedzie wolasz 1 metode)
 - persystencja w ID obiektow
-
+- usuwanie dla teacher + lesson
 
 *sprawdzenie testow
  */
